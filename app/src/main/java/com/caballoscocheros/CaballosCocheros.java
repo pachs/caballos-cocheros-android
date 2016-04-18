bdc24173774623626c7d6b9b39460a637d9f3992
pachs <ma.de1295@uniandes.edu.co>
2016-04-18 07:33:54 -0500

    Versión 2 del proyecto sin OpenCV

--- app/src/main/java/com/caballoscocheros/CaballosCocheros.java
+++ app/src/main/java/com/caballoscocheros/CaballosCocheros.java
@@ -4,15 +4,21 @@ import android.app.AlertDialog;
 import android.app.Application;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.location.Location;
+import android.media.ExifInterface;
 import android.util.Log;
 
 import com.caballoscocheros.util.DatabaseHelper;
-import com.caballoscocheros.util.ObjectRecognition;
+//import com.caballoscocheros.util.ObjectRecognition;
 import com.caballoscocheros.view.MainActivity;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Alvin on 21/02/2016.
@@ -26,7 +32,7 @@ public class CaballosCocheros extends Application {
     /**
      * Instancia del objeto que reconoce imagenes.
      */
-    private ObjectRecognition recon;
+    //private ObjectRecognition recon;
 
     @Override
     public void onCreate() {
@@ -39,28 +45,61 @@ public class CaballosCocheros extends Application {
         return dbHelper;
     }
 
-    public ObjectRecognition getObjectRecon() {
-        if (recon == null) {
-            try{
-                InputStream is = getAssets().open("haar_caballos.xml");
-                File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-                File mCascadeFile = new File(cascadeDir, "haar_caballos.xml");
-                FileOutputStream os = new FileOutputStream(mCascadeFile);
+//    No se usa por el momento
 
-                byte[] buffer = new byte[4096];
-                int bytesRead;
-                while ((bytesRead = is.read(buffer)) != -1) {
-                    os.write(buffer, 0, bytesRead);
-                }
-                is.close();
-                os.close();
-                recon = new ObjectRecognition(mCascadeFile.getAbsolutePath());
-                Log.e("Cargue", "Objeto de reconocimiento creado exitosamente");
-            } catch(Exception e){
-                Log.e("Cargue", e.getMessage());
+//    public ObjectRecognition getObjectRecon() {
+//        if (recon == null) {
+//            try{
+//                InputStream is = getAssets().open("haar_caballos.xml");
+//                File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+//                File mCascadeFile = new File(cascadeDir, "haar_caballos.xml");
+//                FileOutputStream os = new FileOutputStream(mCascadeFile);
+//
+//                byte[] buffer = new byte[4096];
+//                int bytesRead;
+//                while ((bytesRead = is.read(buffer)) != -1) {
+//                    os.write(buffer, 0, bytesRead);
+//                }
+//                is.close();
+//                os.close();
+//                recon = new ObjectRecognition(mCascadeFile.getAbsolutePath());
+//                Log.e("Cargue", "Objeto de reconocimiento creado exitosamente");
+//            } catch(Exception e){
+//                Log.e("Cargue", e.getMessage());
+//            }
+//        }
+//        return recon;
+//    }
+
+    public float[] getLatLng(String ruta)throws Exception{
+        try {
+            ExifInterface exif = new ExifInterface(ruta);
+            float[] latlng = new float[2];
+            if(exif.getLatLong(latlng)){
+                return latlng;
             }
+            else{
+                //throw new Exception("No fue posible extraer la latitud y longitud.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return recon;
+        return null;
+    }
+
+    public Date getImageDate(String ruta){
+        try{
+            ExifInterface exif = new ExifInterface(ruta);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
+            String fecha = exif.getAttribute(ExifInterface.TAG_DATETIME);
+            return sdf.parse(fecha);
+
+        } catch(IOException e){
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public void onLowMemory() {
--- app/src/main/java/com/caballoscocheros/view/MainActivity.java
+++ app/src/main/java/com/caballoscocheros/view/MainActivity.java
@@ -1,47 +1,202 @@
 package com.caballoscocheros.view;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.caballoscocheros.CaballosCocheros;
 import com.caballoscocheros.R;
 import com.caballoscocheros.util.SingleClickActionListener;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.squareup.picasso.Picasso;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
+//import org.opencv.android.BaseLoaderCallback;
+//import org.opencv.android.LoaderCallbackInterface;
+//import org.opencv.android.OpenCVLoader;
+
+import org.openalpr.OpenALPR;
+import org.openalpr.model.Results;
+import org.openalpr.model.ResultsError;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Alvin on 14/02/2016.
+ * Ejemplo de Openalpr tomado de https://github.com/SandroMachado/openalpr-android
  */
-public class MainActivity extends BaseDrawerActivity {
+public class MainActivity extends BaseDrawerActivity implements OnMapReadyCallback{
 
-    static {
-        if (!OpenCVLoader.initDebug()) {
-            Log.e("Error", "OpenCV no se pudo inicializar.");
-        }
-    }
+    //static {
+    //    if (!OpenCVLoader.initDebug()) {
+    //        Log.e("Error", "OpenCV no se pudo inicializar.");
+    //    }
+    //}
 
     private final static String TAG = "MainActivity";
+    private static final int REQUEST_IMAGE = 100;
+
+    private String ANDROID_DATA_DIR;
+    final int STORAGE=1;
+    private static File destination;
+    private TextView resultTextView;
+    private ImageView imageView;
+    private GoogleMap mapa;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setActivityContent(R.layout.content_main);
 
+        //Extraer el directorio de la aplicación
+        ANDROID_DATA_DIR = this.getApplicationInfo().dataDir;
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_main);
         fab.setOnClickListener(new SingleClickActionListener() {
             @Override
             public void onSingleClick(View v) {
-                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+                checkPermission();
+                //startActivity(new Intent(MainActivity.this, CameraActivity.class));
             }
         });
 
         //Tratar de cargar el reconocedor antes de que la cámara mande frames
         CaballosCocheros mApp = (CaballosCocheros) getApplication();
-        mApp.getObjectRecon();
+
+        //No hay reconocimiento de objetos por el momento
+        //mApp.getObjectRecon();
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    private void checkPermission() {
+        List<String> permissions = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissions.isEmpty()) {
+            Toast.makeText(this, "Storage access needed to manage the picture.", Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            ActivityCompat.requestPermissions(this, params, STORAGE);
+        } else { // We already have permissions, so handle as normal
+            takePicture();
+        }
+    }
+
+    public void takePicture(){
+        // Use a folder to store all results
+        File folder = new File(Environment.getExternalStorageDirectory() + "/OpenALPR/");
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
+        // Generate the path for the next photo
+        String name = dateToString(new Date(), "yyyy-MM-dd-hh-mm-ss");
+        destination = new File(folder, name + ".jpg");
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(destination));
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE && resultCode == Activity.RESULT_OK) {
+            final ProgressDialog progress = ProgressDialog.show(this, "Loading", "Parsing result...", true);
+            final String openAlprConfFile = ANDROID_DATA_DIR + File.separatorChar + "runtime_data" + File.separatorChar + "openalpr.conf";
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 10;
+
+            // Picasso requires permission.WRITE_EXTERNAL_STORAGE
+            Picasso.with(MainActivity.this).load(destination).fit().centerCrop().into(imageView);
+            resultTextView.setText("Processing");
+
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    String result = OpenALPR.Factory.create(MainActivity.this, ANDROID_DATA_DIR).recognizeWithCountryRegionNConfig("us", "", destination.getAbsolutePath(), openAlprConfFile, 10);
+
+                    Log.d("OPEN ALPR", result);
+
+                    try {
+                        final Results results = new Gson().fromJson(result, Results.class);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (results == null || results.getResults() == null || results.getResults().size() == 0) {
+                                    Toast.makeText(MainActivity.this, "It was not possible to detect the licence plate.", Toast.LENGTH_LONG).show();
+                                    resultTextView.setText("It was not possible to detect the licence plate.");
+                                } else {
+                                    resultTextView.setText("Plate: " + results.getResults().get(0).getPlate()
+                                            // Trim confidence to two decimal places
+                                            + " Confidence: " + String.format("%.2f", results.getResults().get(0).getConfidence()) + "%"
+                                            // Convert processing time to seconds and trim to two decimal places
+                                            + " Processing time: " + String.format("%.2f", ((results.getProcessing_time_ms() / 1000.0) % 60)) + " seconds");
+                                }
+                            }
+                        });
+
+                    } catch (JsonSyntaxException exception) {
+                        final ResultsError resultsError = new Gson().fromJson(result, ResultsError.class);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                resultTextView.setText(resultsError.getMsg());
+                            }
+                        });
+                    }
+
+                    progress.dismiss();
+                }
+            });
+        }
+
+    }
+
+
+    public String dateToString(Date date, String format) {
+        SimpleDateFormat df = new SimpleDateFormat(format, Locale.getDefault());
+
+        return df.format(date);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
+    }
+
+    public void ponerMarcador(double lat, double lng, String titulo){
+        mapa.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(titulo));
+    }
+
 }
